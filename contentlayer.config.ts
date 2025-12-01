98047cb900e339567a5d1402b73ad606ce99dae6
NilFoundation <63165931+Xcertik-Realist@users.noreply.github.com>
2025-12-01 14:51:25 +0000

    Add contentlayer configuration for Post documents

---
+++ contentlayer.config.ts
@@ -0,0 +1,24 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+
+export const Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `posts/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    description: { type: "string", required: true },
+    date: { type: "date", required: true },
+    readingTime: { type: "number", required: true },
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (post) => post._raw.flattenedPath.replace("posts/", ""),
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "posts",
+  documentTypes: [Post],
+});
