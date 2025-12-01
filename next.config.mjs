0b3d46eb5ac6ac04205a9a4ab62f64645bfbc75a
NilFoundation <63165931+Xcertik-Realist@users.noreply.github.com>
2025-12-01 14:50:54 +0000

    Add Next.js configuration with contentlayer support

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "images.pexels.com"
+    ],
+  },
+};
+
+export default withContentlayer(nextConfig);
